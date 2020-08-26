f5394471d82e3f4dc5a3654905cf488283b690f7
Allison Usher <allyjay317@gmail.com>
2020-08-25 20:04:36 -0700

    added login/logout/signup buttons to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,32 @@
-import React from 'react'
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
+import { AppBar, Container, Toolbar, Typography, Button } from '@material-ui/core'
+import { useHistory } from 'react-router'
 
 const Header = () => {
+  const history = useHistory()
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('token') !== null)
   return (
-      <AppBar position="static" color="primary">
-        <Container maxWidth="md">
-          <Toolbar>
-            <Typography variant="h3" color="inherit">
-              Spotify Song Suggestor
+    <AppBar position="static" color="primary">
+      <Container maxWidth="md">
+        <Toolbar>
+          <Typography style={{ flexGrow: 1 }} variant="h3" color="inherit">
+            Spotify Song Suggestor
             </Typography>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    )
+          {loggedIn ?
+            <Button onClick={() => {
+              localStorage.removeItem('token')
+              setLoggedIn(false)
+              history.push('/')
+            }}>Sign Out</Button>
+            :
+            <>
+              <Button onClick={() => history.push('/sign-up')}>Sign Up</Button>
+              <Button onClick={() => history.push('/')}>Log In</Button>
+            </>}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
 }
 
 export default Header
